d8449c71780a6174570d735228ce7446e2ad86e7
GeForceLegend <41003546+GeForceLegend@users.noreply.github.com>
2026-08-11 11:34:19 +0800

    Adjusted default water wave

--- shaders/libs/Water.glsl
+++ shaders/libs/Water.glsl
@@ -1,7 +1,7 @@
 #define WATER_WAVE_SCALE 1.0 // [0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.4 1.8 1.9 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0 4.2 4.4 4.6 4.8 5.0 5.5 6.0 6.5 7.0 7.5 8.0 9.5 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0]
-#define WATER_WAVE_HEIGHT 1.0 // [0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.4 1.8 1.9 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0 4.2 4.4 4.6 4.8 5.0 5.5 6.0 6.5 7.0 7.5 8.0 9.5 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0]
+#define WATER_WAVE_HEIGHT 0.9 // [0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0 1.1 1.2 1.3 1.4 1.5 1.6 1.4 1.8 1.9 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0 4.2 4.4 4.6 4.8 5.0 5.5 6.0 6.5 7.0 7.5 8.0 9.5 10.0 11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0]
 #define WATER_WAVE_STEEPNESS 0.4 // [0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0]
-#define WATER_WAVE_LAYERS_SCALE 1.55 // [1.05 1.1 1.15 1.2 1.25 1.3 1.35 1.4 1.45 1.5 1.55 1.6 1.65 1.7 1.75 1.8 1.85 1.9 1.95 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0]
+#define WATER_WAVE_LAYERS_SCALE 1.4 // [1.05 1.1 1.15 1.2 1.25 1.3 1.35 1.4 1.45 1.5 1.55 1.6 1.65 1.7 1.75 1.8 1.85 1.9 1.95 2.0 2.1 2.2 2.3 2.4 2.5 2.6 2.7 2.8 2.9 3.0]
 #define WATER_WAVE_LAYERS_FADE 0.6 // [0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 1.0]
 #define WATER_WAVE_HEIGHT_LAYERS 3 // [1 2 3 4 5 6 7 8 9 10]
 #define WATER_WAVE_NORMAL_LAYERS 5 // [1 2 3 4 5 6 7 8 9 10 12 14 16 18 20]
@@ -21,7 +21,7 @@ float gerstnerApproxGeForceLegend(vec2 coord) {
 }
 
 float waterWaveHeight(vec2 coord) {
-    coord *= 30.0;
+    coord *= 32.0;
     float totalHeight = 0.0;
     float currentWeight = 1.0;
     float speed = 2.0 * frameTimeCounter * WATER_WAVE_SPEED;
@@ -59,9 +59,9 @@ vec2 gerstnerApproxGeForceLegendDerivative(vec2 coord) {
 
 vec2 waterWaveNormal(vec2 coord) {
     vec2 totalNormal = vec2(0.0);
-    mat2 normalRotation = transpose(goldenRotate) * mat2(30.0);
+    mat2 normalRotation = transpose(goldenRotate) * mat2(32.0);
     const mat2 layerRotation = mat2(WATER_WAVE_LAYERS_SCALE) * mat2(goldenRotate[0].x, goldenRotate[1].x, goldenRotate[0].y, goldenRotate[1].y);
-    coord *= 30.0;
+    coord *= 32.0;
     float totalHeight = 0.0;
     float currentWeight = 1.0;
     float speed = 2.0 * frameTimeCounter * WATER_WAVE_SPEED;
